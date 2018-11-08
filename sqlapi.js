133d5c2e24b15438114438999ea203be4d4bbb2e
Mazinger <ricardojmv85@gmail.com>
2018-11-07 22:37:07 -0600

    q

---
+++ sqlapi.js
@@ -0,0 +1,33 @@
+var request = require('request')
+const rp = require('request-promise')
+const express = require('express')
+const app = express()
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'circuitos.cr0c2d7y40q0.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'root',
+    password: 'root1234',
+    database: 'progra'
+  })
+
+  app.get("/:id", (req, res) => {
+    const id = req.params.id
+    var values="";
+    var arr = id.split(",")
+    values+="("+arr[0]+",'2018-10-29')"
+    if (arr.length>1){
+        for (var i=1;i<arr.length;i++){
+            values+=",("+arr[i]+",'2018-10-29')"
+        }
+    }
+    console.log(values);    
+    const queryString = "INSERT INTO reg_temp (id_vaca, fecha_registro) VALUES"+values
+    connection.query(queryString, function (err, result) {
+        if (err) throw err;
+      });
+  })
+  app.listen(3003, () => {
+    console.log("Server is up and listening on 3003...")
+  })
